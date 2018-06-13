7c716c4909a524d6db73673f01abeee173703242
RyanGan <ryan.william.gan@gmail.com>
2018-06-13 18:03:27 -0400

    added remote hose to app.run

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def about():
 #if __name__ == '__main__':
 #    app.run(host='0.0.0.0',port=33507, debug=True)
 if __name__ == '__main__':
-  app.run(port=33507, debug=True)
+  app.run(host='0.0.0.0', port=33507, debug=True)
